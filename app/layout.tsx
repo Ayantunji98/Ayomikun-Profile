6b64e95f80beaf56136b859d6c1339c7000e60b2
Ayantunji98 <ayomikunayantunji98@gmail.com>
2025-09-19 15:34:25 +0100

    Done

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+import { GeistSans } from "geist/font/sans"
+import { GeistMono } from "geist/font/mono"
+import { Analytics } from "@vercel/analytics/react"
+import { Suspense } from "react"
+
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
+      <body>
+        <Suspense fallback={null}>{children}</Suspense>
+        <Analytics />
+      </body>
+    </html>
+  )
+}
